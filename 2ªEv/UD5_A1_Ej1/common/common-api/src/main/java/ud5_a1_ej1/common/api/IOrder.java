1d4118264777d165edf3a0ab2ad58ba8901630aa
Jorge del Hoyo <delhoyoballestin23@iespabloserrano.com>
2026-02-01 17:57:10 +0100

    Limpieza de directorios

--- 2ªEv/UD5_A1_Ej1/common/common-api/src/main/java/ud5_a1_ej1/common/api/IOrder.java
+++
@@ -1,5 +0,0 @@
-package ud5_a1_ej1.common.api;
-
-public interface IOrder {
-	void procesar();
-}
--- 2ªEv/UD5_A1_Ej1/common/common-utils/src/main/java/ud5_a1_ej1/common/utils/LogUtils.java
+++
@@ -1,7 +0,0 @@
-package ud5_a1_ej1.common.utils;
-
-public class LogUtils {
-	public static void log(String mensaje) {
-		System.out.println("[LOG]: "+mensaje);
-	}
-}
